ed515483042b1bed47e142f1b71733086bec4d33
Carolyn Tambini <carolyn@codeandtheory.com>
2018-01-24 00:40:11 -0500

    qa grid updates and new images

--- qa.js
+++ qa.js
@@ -1,11 +1,50 @@
-var image_tracker ='adam';
-
 function change(){
-	var image=document.getElementById('adam');
-	if (image_tracker=="adam") {
-	  image.src= "file:///Users/carolyntambini/Code/qa-sandbox/images/QA/adamadult.jpg";
-	}else {
-	   image.src="adam.jpg";
-	}
-}
+	var image = document.getElementById('adam');
+	image.src = "adamadult.jpg";
 
+
+	var image = document.getElementById('carolyn');
+	image.src = "carolynadult.jpg";
+
+
+
+	var image = document.getElementById('cassie');
+	image.src = "cassiedult.jpg";
+
+
+	var image = document.getElementById('gina');
+	image.src = "ginaadult.jpg";
+
+
+	var image = document.getElementById('james');
+	image.src = "jamesadult.jpg";
+
+
+	var image = document.getElementById('johnny');
+	image.src = "johnnyadult.jpg";
+
+
+	var image = document.getElementById('megan');
+	image.src = "meganadult.jpg";
+
+
+	var image = document.getElementById('pete');
+	image.src = "peteadult.jpg";
+
+
+	var image = document.getElementById('sara');
+	image.src = "saraadult.jpg";
+
+
+	var image = document.getElementById('sammy');
+	image.src = "sammyadult.jpg";
+
+
+	var image = document.getElementById('jenny');
+	image.src = "jennydult.jpg";
+
+
+	var image = document.getElementById('alex');
+	image.src = "alexadult.jpg";
+
+}
