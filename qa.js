e97972a2ce5556f168b3064c4108a27575792efc
Carolyn Tambini <carolyn@codeandtheory.com>
2018-01-23 19:01:49 -0500

    qa grid updates

---
+++ qa.js
@@ -0,0 +1,11 @@
+var image_tracker ='adam';
+
+function change(){
+	var image=document.getElementById('adam');
+	if (image_tracker=="adam") {
+	  image.src= "file:///Users/carolyntambini/Code/qa-sandbox/images/QA/adamadult.jpg";
+	}else {
+	   image.src="adam.jpg";
+	}
+}
+
